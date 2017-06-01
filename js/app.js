c7e6fc53b28a6a81287b0415e93a9c44a8a1878d
magentaFire <dafne.magenta@gmail.com>
2017-06-01 14:03:54 -0500

    lista version 0.0.1 con materialize y jquery

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
-var contador = 1;
+(function(){
 
-$(document).ready(iniciaFuncion);
+  var contador = 1;
 
-function iniciaFuncion() {
-  $(".btn-publicar").click(publicaTweet);
-}
+  $(document).ready(iniciaFuncion);
 
-function publicaTweet(texto){
-  // texto = $(".tweet-edit").val()
-  // $(".nuevos-tweets").append(texto);
-  $("<div></div>").appendTo(".nuevos-tweets").addClass("col s12 m6 nuevo-tweet" + contador);
-  $("<div></div>").appendTo(".nuevo-tweet" + contador).addClass("card blue-grey darken-1 nueva-card" + contador);
-  $("<div></div>").appendTo(".nueva-card" + contador).addClass("card-content white-text nuevo-content" + contador);
-  $("<span></span>").appendTo(".nuevo-content" + contador).text($(".tweet-title").val()).addClass("card-title");;
-  $("<p></p>").appendTo(".nuevo-content" + contador).text($(".tweet-edit").val());
+  function iniciaFuncion() {
+    $(".btn-publicar").click(publicaTweet);
+  }
 
-  borraCampos();
-  contador += 1;
-}
+  function publicaTweet(){
+    var $texto = $(".tweet-edit").val();
+    var $titulo = $(".tweet-title").val()
+    $("<div></div>").prependTo(".nuevos-tweets").addClass("col s12 m6 nuevo-tweet" + contador);
+    $("<div></div>").appendTo(".nuevo-tweet" + contador).addClass("card blue-grey darken-1 nueva-card" + contador);
+    $("<div></div>").appendTo(".nueva-card" + contador).addClass("card-content white-text nuevo-content" + contador);
+    $("<div></div>").appendTo(".nueva-card" + contador).addClass("card-action eliminar-container" + contador);
+    $("<span></span>").appendTo(".eliminar-container" + contador).addClass("btn-x eliminar-btn" + contador).text("Eliminar");
+    $("<span></span>").appendTo(".nuevo-content" + contador).text($titulo).addClass("card-title");
+    $("<p></p>").appendTo(".nuevo-content" + contador).text($texto);
+    borraCampos();
+    contador += 1;
+  }
 
-function borraCampos(){
-  $(".tweet-edit").val("");
-  $(".tweet-title").val("");
+  function borraCampos(){
+    $(".tweet-edit").val("");
+    $(".tweet-title").val("");
 
-}
+  }
+}());
